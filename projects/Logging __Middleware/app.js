2cf6d38661e95ab71444657758c444d2c6d8b7e5
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-07-07 23:42:12 +0530

    Logging__Middleware create

---
+++ projects/Logging __Middleware/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import loggingMiddleware from './middlewares/loggingMiddleware.js';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.use(loggingMiddleware);
+connectDB()
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({
+        message: err.message
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
