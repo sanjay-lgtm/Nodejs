6e8a7b898ac1d63b708c34c302b97d022d5a6315
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-07-13 00:49:35 +0530

    poppeteer added

--- projects/Basic_poppeeteer/app.js
+++ projects/Basic_poppeeteer/app.js
@@ -1,18 +1,29 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({headless:true});
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-     await page.goto("https://www.flipkart.com/search?q=iphone+14");
+    await page.goto("https://www.flipkart.com/search?q=iphone+14");
 
     //Set screen size.
-    // await page.setViewport({width: 1080, height: 1024});
+    await page.setViewport({ width: 1080, height: 1024 });
 
     //Take screenShot
-    // await page.screenshot({path: 'screenshot.png'});
+    await page.screenshot({path: 'screenshot.png'});
 
     //Create PDF
-    // await page.pdf({path: 'screenshot.pdf', format: 'A4'});
+    await page.pdf({path: 'screenshot.pdf', format: 'A4'});
+
+    //select element
+    const elements  = await page.$$('.CGtC98');
+
+    for (let ele of elements){
+        const text = await page.evaluate((ele)=> ele.textContent,ele);
+        console.log(text)
+    }
+
+    //close the browser
+    await browser.close();
 })();
